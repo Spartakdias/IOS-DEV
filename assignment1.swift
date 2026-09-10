b9c84bb7e66d7b0a828c623bfc55322411a1123e
Dias <d.kadyraly@gmail.com>
2026-09-10 09:16:44 +0500

    assignment1

---
+++ assignment1.swift
@@ -0,0 +1,35 @@
+let firstName: String = "Dias"
+let lastName: String = "Kadyraly"
+let birthYear: Int = 2006
+let currentYear: Int = 2026
+let age: Int = currentYear - birthYear
+
+let isStudent: Bool = true
+let height: Double = 1.87
+
+let hobby: String = "playing videogames"
+let numberOfHobbies: Int = 3
+let favoriteNumber: Int = 24
+let isHobbyCreative: Bool = true
+let favoriteSport: String = "Football"
+let favoriteFood: String = "Pizza"
+
+let futureGoals: String = "I want to become a professional football player."
+let favoriteEmoji: String = "⚽"
+
+let lifeStory = """
+My name is \(firstName) \(lastName).
+I am \(age) years old and I was born in \(birthYear).
+I am currently a student: \(isStudent).
+My height is \(height) meters.
+My favorite hobby is \(hobby).
+I have \(numberOfHobbies) hobbies in total.
+My favorite number is \(favoriteNumber).
+Is my hobby creative? \(isHobbyCreative).
+My favorite sport is \(favoriteSport), and my favorite food is \(favoriteFood).
+In the future, \(futureGoals)
+My favorite emoji is \(favoriteEmoji).
+"""
+
+
+print(lifeStory)
